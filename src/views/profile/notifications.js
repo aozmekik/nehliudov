178b48e05d11fc8b2c186a05e3891489c4a14243
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2021-05-17 12:04:17 +0300

    refactor: seperated styles

--- src/views/profile/notifications.js
+++ src/views/profile/notifications.js
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { ScrollView, Image, View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 import NavBar from '../../components/nav-bar';
 
 function NotificationItem() {
     return (
-        <TouchableOpacity style={{ flexDirection: 'row', marginLeft: 15, marginTop: 20 }}>
-            <Image style={{ width: 51, height: 51 }} source={require('../../icons/woman.png')} />
-            <View style={{ marginLeft: 15, width: '75%' }}>
-                <Text style={{ fontFamily: 'SFProText-Bold', fontSize: 12, color: '#0A151F' }}>Özge Yılmaz</Text>
-                <Text style={{ fontFamily: 'SFProText-Medium', fontSize: 12, color: '#0A151F' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus faucibus, elit nec euismod viverra.</Text>
+        <TouchableOpacity style={styles.container}>
+            <Image style={styles.profile} source={require('../../icons/woman.png')} />
+            <View style={styles.rightSection}>
+                <Text style={styles.title}>Özge Yılmaz</Text>
+                <Text style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus faucibus, elit nec euismod viverra.</Text>
             </View>
         </TouchableOpacity>
     )
@@ -28,4 +28,29 @@ function NotificationsScreen({ navigation }) {
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginLeft: 15,
+        marginTop: 20
+    },
+    profile: {
+        width: 51,
+        height: 51
+    },
+    rightSection: {
+        marginLeft: 15,
+        width: '75%'
+    },
+    title: {
+        fontFamily: 'SFProText-Bold',
+        fontSize: 12,
+        color: '#0A151F'
+    },
+    desc: {
+        fontFamily: 'SFProText-Medium',
+        fontSize: 12,
+        color: '#0A151F'
+    }
+});
 export default NotificationsScreen;
--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -12,7 +12,7 @@ const Stack = createStackNavigator();
 
 function ImageBox({ style }) {
     return (
-        <TouchableOpacity style={{ ...style, width: '33%' }}>
+        <TouchableOpacity style={{ ...style, ...styles.imageBox }}>
             <Image style={styles.image} source={require('../../icons/photo.png')} />
         </TouchableOpacity>
     )
@@ -21,21 +21,21 @@ function ImageBox({ style }) {
 function Main({ navigation }) {
     return (
         <>
-            <View style={{ marginTop: 50, marginLeft: 15, flexDirection: 'row' }} >
-                <Image style={{ width: 51, height: 51 }} source={require('../../icons/woman.png')} />
-                <View style={{ marginHorizontal: 15 }}>
-                    <View style={{ flex: 1, flexDirection: 'row', }} >
-                        <Text style={{ fontFamily: 'SFProText-Bold', fontSize: 24, color: '#0A151F', flexDirection: 'row' }}>Özge Yılmaz</Text>
-                        <TouchableOpacity onPress={() => navigation.navigate('Notifications')} style={{ marginTop: 5, marginLeft: 50, marginRight: 10 }}>
+            <View style={styles.header} >
+                <Image style={styles.profile} source={require('../../icons/woman.png')} />
+                <View style={styles.rightSection}>
+                    <View style={styles.firstRow} >
+                        <Text style={styles.name}>Özge Yılmaz</Text>
+                        <TouchableOpacity style={styles.notification} onPress={() => navigation.navigate('Notifications')} >
                             <Notification />
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={{ marginTop: 5 }}><Settings /></TouchableOpacity>
+                        <TouchableOpacity style={styles.settings} onPress={() => navigation.navigate('Settings')} ><Settings /></TouchableOpacity>
                     </View>
-                    <Text style={{ fontFamily: 'SFProText-MediumItalic', fontSize: 12, color: '#0A151F' }}>Ümraniye Temsilcisi</Text>
+                    <Text style={styles.name}>Ümraniye Temsilcisi</Text>
                 </View>
             </View>
 
-            <ScrollView style={{ marginTop: 15 }} showsVerticalScrollIndicator={false} >
+            <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false} >
                 <View style={{ flexDirection: 'row' }}>
                     <ImageBox />
                     <ImageBox style={{ marginHorizontal: 2.5 }} />
@@ -68,7 +68,6 @@ function ProfileScreen() {
             <Stack.Screen name='Main' component={Main} />
             <Stack.Screen name='Notifications' component={NotificationsScreen} />
             <Stack.Screen name='Settings' component={SettingsScreen} />
-
         </Stack.Navigator>
     )
 }
@@ -79,6 +78,47 @@ const styles = StyleSheet.create({
         height: undefined,
         aspectRatio: 1,
     },
+    imageBox: {
+        width: '33%'
+    },
+    header: {
+        marginTop: 50,
+        marginLeft: 15,
+        flexDirection: 'row'
+    },
+    profile: {
+        width: 51,
+        height: 51
+    },
+    rightSection: {
+        marginHorizontal: 15
+    },
+    firstRow: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    name: {
+        fontFamily: 'SFProText-Bold',
+        fontSize: 24,
+        color: '#0A151F',
+        flexDirection: 'row'
+    },
+    notification: {
+        marginTop: 5,
+        marginLeft: 50,
+        marginRight: 10
+    },
+    settings: {
+        marginTop: 5
+    },
+    title: {
+        fontFamily: 'SFProText-MediumItalic',
+        fontSize: 12,
+        color: '#0A151F'
+    },
+    scrollView: {
+        marginTop: 15
+    }
 });
 
 export default ProfileScreen;
